2987d54a9a2c1d5c4f72011bfdae5f7146da53c9
ascii <39356292+asciidude@users.noreply.github.com>
2022-06-23 14:52:45 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,13 +1,29 @@
 --[[
     DO NOT REUPLOAD WITHOUT MY PERMISSION
     Thank you for using my script
+
+    ----
+
+    Infinite will ignore:
+    * AFK passive
+    * Message (notification) settings, even if enabled - THIS EXCLUDES WELCOME MESSAGE
+    * Time settings
+
+    It will start on join, non-configurable
+
+    ----
+
+    WARNING | THIS SCRIPT DOES NOT STOP VDM WHATSOEVER, PLEASE KEEP IN MIND
+    WARNING | PASSIVE MODE ENABLES GODMODE, YOU SHOULD ONLY ALLOW AFK PASSIVE TO TRUSTED MEMBERS
+    WARNING | This script may also have lots of bugs or issues, if so please DM me on Discord: asciidude#0001
 ]]--
 
 config = {}
 
 config.serverName = "Virginia State Roleplay"
+config.infinite = false -- Infinitely enable peacetime upon join, heavily recommended against using
 
-config.enabled = true -- Enable or disable the plugin
+config.joinEnabled = true -- Enable or disable join passive
 config.time = 120 * 100 -- Time in seconds (x * 100)
 
 config.messageEnabled = true -- Determine if you'd like to show a message to the player about Peace Mode
@@ -18,3 +34,14 @@ config.endMessage = "~g~Passive Mode has been disabled, you can now resume rolep
 
 config.welcomeEnabled = false -- Welcome the player upon joining
 config.welcomeMessage = "~g~Welcome to " .. config.serverName
+
+
+config.AFKpassiveEnabled = true -- Enable or disable AFK passive, command-only (can't figure out automatic afk)
+
+config.AFKpassiveCommand = "afk" -- The command to run AFK passive
+config.enableACE = false -- Enable ACE permission for AFK passive (command.afk)
+
+config.AFKpassiveMessageEnabled = true -- Determines if you want a message to be shown to people when they use /afk
+config.AFKpassiveEnabledMessage = "~g~You are now AFK" -- The message to show when enabled
+config.AFKpassiveDisabledMessage = "~r~You are no longer AFK" -- The message to show when disabled
+config.AFKpassiveFailureMessage = "~r~Unable to make you AFK!" -- The message will be shown when join passive is active
