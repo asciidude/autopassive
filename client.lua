7d35a71c6596da07d3953b104bf3adde5d88e441
ascii <39356292+asciidude@users.noreply.github.com>
2022-06-23 11:11:37 -0500

    better memory management and stuff :')

--- client.lua
+++ client.lua
@@ -9,45 +9,82 @@ function enablePeaceMode(player)
     SetPlayerCanDoDriveBy(player, false)
     DisablePlayerFiring(player, true)
     DisableControlAction(0, 140)
+    SetEntityInvincible(player, true)
 end
 
--- CODE --
+-- Join Passive --
 print('AutoPassive loaded, created by asciidude')
-local time = config.time
+local joinPassive = false
 
-if config.enabled == true then
+if config.joinEnabled then
+    joinPassive = true
     AddEventHandler('playerSpawned', function()
         if config.welcomeEnabled then
             notify(config.message)
         end
 
-        if config.messageEnabled then
-            notify(config.message)
-        end
-        
-        Citizen.CreateThread(function()
-            local ped = GetPlayerPed(-1)
-            
-            while time > 0 do
-                Citizen.Wait(1)
-                enablePeaceMode(ped)
+        if config.infinite then
+            Citizen.CreateThread(function()
+                local ped = GetPlayerPed(-1)
 
-                if config.messageEnabled then
-                    if IsControlJustReleased(1, 24) then
-                        notify(config.message)
+                while true do
+                    Citizen.Wait(1)
+                    enablePeaceMode(ped)
+                end
+            end)
+        else
+            local time = config.time
+
+            if config.messageEnabled then
+                notify(config.message)
+            end
+            
+            Citizen.CreateThread(function()
+                local ped = GetPlayerPed(-1)
+                
+                while time > 0 do
+                    Citizen.Wait(1)
+                    enablePeaceMode(ped)
+
+                    time = time - 1
+                end
+                
+                -- END --
+                if time == 0 then
+                    if config.endMessageEnabled then
+                        notify(config.endMessage)
                     end
-                end
 
-                time = time - 1
-                print(time)
-            end
-            
-            -- END --
-            if time == 0 then
-                if config.endMessageEnabled then
-                    notify(config.endMessage)
+                    SetEntityInvincible(ped, false)
+                    joinPassive = false
+                end
+            end)
+        end
+    end)
+end
+
+-- AFK Passive --
+if config.AFKpassiveEnabled and config.infinite == false then
+    local ped = GetPlayerPed(-1)
+    local AFK = false
+
+    RegisterCommand(config.AFKpassiveCommand, function(source, args, rawCommand)
+        if joinPassive then
+            notify(config.AFKpassiveFailureMessage)
+        else
+            if AFK then
+                AFK = false
+                notify(config.AFKpassiveDisabledMessage)
+                SetEntityInvincible(ped, false)
+            else
+                AFK = true
+                notify(config.AFKpassiveEnabledMessage)
+
+                while AFK do
+                    Citizen.Wait(1)
+                    enablePeaceMode(ped)
                 end
             end
-        end)
-    end)
+        end
+    end, config.enableACE)
 end
